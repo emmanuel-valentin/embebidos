e52c97869d41cf9ebe4f76cf38b1660a3e50a954
Emmanuel Valentin <ema_110@outlook.es>
2023-11-17 21:32:54 -0600

    Práctica 3

--- Practica3/freq_div.v
+++ Practica3/freq_div.v
@@ -4,19 +4,19 @@ module freq_div(
 	output	reg		clk_div
 );
 
-	reg	[25:0] 	count;
+	reg	[31:0] 	count;
 	
 	always @(posedge clk_i, negedge rst_i)
 	begin
 		if (!rst_i)
 		begin
-			count = 26'b0;
+			count = 32'd0;
 			clk_div = 1'b0;
 		end
 		else
-			if (count == 26'd6_999_999)
+			if (count == 32'd6_999_999)
 			begin
-				count = 26'b0;
+				count = 32'd0;
 				clk_div = ~clk_div;
 			end
 			else
--- Practica3/semaforo.v
+++ Practica3/semaforo.v
@@ -1,19 +1,20 @@
 module semaforo #(parameter initial_state = 2'd0) 
 (
-	input				clk_i,
-	input				rst_i,
-	// TODO: Implementar el push button para dar paso al peaton
-	input				sw_green_i,
-	output reg		red,
-	output reg		green,
-	output reg		yellow
+	input clk_i,
+	input rst_i,
+	input sw_green_i,
+	output reg red,
+	output reg green,
+	output reg yellow
 );
 
-	reg	[1:0]	current_state;
-	reg	[1:0]	next_state;
-	reg	[6:0]	count;
-	reg	[6:0]	start;
-	reg   [1:0] prev_count;
+	reg [1:0] current_state;
+	reg [1:0] next_state;
+	reg [6:0] count;
+	reg [6:0] start;
+
+	reg [6:0] tmp_count = 7'd0;
+	reg sw_green_p;
 	
 	initial
 	begin
@@ -25,23 +26,24 @@ module semaforo #(parameter initial_state = 2'd0)
 			start = 7'd60;
 	end
 	
-	always @(posedge clk_i, negedge rst_i)
-	begin
+	always @(posedge clk_i, posedge sw_green_p, negedge rst_i)
 		if (!rst_i)
-			current_state = initial_state;
+			current_state <= initial_state;
 		else
-			current_state = next_state;
-	end
+			current_state <= next_state;
+			
 	
 	always @(posedge clk_i, negedge rst_i)
 	begin
 		if (!rst_i)
-			count = start;
+		begin
+			count <= start;
+		end
 		else
 		begin
 			if (count == 7'd119)
-				count = 7'd0;
-			count = count + 1;
+				count <= 7'd0;
+			count <= count + 1;
 		end
 	end
 	
@@ -51,34 +53,40 @@ module semaforo #(parameter initial_state = 2'd0)
 	begin
 		case (current_state)
 			2'd0:
-				if (count >= 7'd54)
-					next_state = 2'd1;
-				else if (!sw_green_i == 1'b1)
+				if (!sw_green_i)
 				begin
-					prev_count = count;
-					next_state = 7'd3;
+					sw_green_p <= 1'b1;
+					tmp_count <= count + 5;
+					next_state <= 2'd3;
 				end
+				else if (count >= 7'd54)
+					next_state <= 2'd1;
 				else
-					next_state = current_state;
+					next_state <= current_state;
 			2'd1:
 				if (count >= 7'd59)
-					next_state = 2'd2;
+					next_state <= 2'd2;
 				else
-					next_state = current_state;
+					next_state <= current_state;
+			// TODO: Moverse al estado en rojo si el botón ha sido presionado y contar los 5seg.
 			2'd2:
-				if (count >= 7'd119)
-					next_state = 2'd0;
-				else
-					next_state = current_state;
-			2'd3:
 			begin
-				if (count == prev_count + 5)
-					next_state = 2'd0;
+				if (count >= 7'd119)
+					next_state <= 2'd0;
 				else
-					next_state = current_state;
+					next_state <= current_state;
 			end
+			2'd3:
+				if (sw_green_p && count == tmp_count)
+				begin
+					sw_green_p <= 1'b0;
+					tmp_count <= 7'd0;
+					next_state <= 2'd0;
+				end
+				else
+					next_state <= current_state;
 			default:
-				next_state = initial_state;
+				next_state <= initial_state;
 		endcase
 	end
 	
@@ -117,34 +125,43 @@ module semaforo #(parameter initial_state = 2'd0)
  endmodule 
 
 
-//module tb_semaforo();
-//
-//	reg				clk_i;
-//	reg				rst_i;
-//	reg				sw_green_i;
-//	wire				red;
-//	wire				green;
-//	wire				yellow;
-//	
-//	
-//	semaforo #(.initial_state(2'd2)) DUT (
-//		.clk_i(clk_i),
-//		.rst_i(rst_i),
-//		.red(red),
-//		.green(green),
-//		.yellow(yellow)
-//	);
-//	
-//	initial clk_i = 1'b0;
-//	always #10 clk_i = ~clk_i;
-//	
-//	initial
-//	begin
-//		rst_i = 1'b0;
-//		sw_green_i = 1'b0;
-//		#10
-//		rst_i = 1'b1;
-//	end
-//
-//	initial #2400 $finish;
-//endmodule 
+module tb_semaforo();
+
+	reg clk_i;
+	reg rst_i;
+	reg sw_green_i;
+	wire red;
+	wire green;
+	wire yellow;
+	
+	
+	semaforo #(.initial_state(2'd0)) DUT (
+		.clk_i(clk_i),
+		.rst_i(rst_i),
+		.sw_green_i(sw_green_i),
+		.red(red),
+		.green(green),
+		.yellow(yellow)
+	);
+	
+	initial clk_i = 1'b1;
+	always #10 clk_i = ~clk_i;
+	
+	initial
+	begin
+		rst_i = 1'b0;
+		sw_green_i = 1'b0;
+		#10
+		rst_i = 1'b1;
+		#5
+		sw_green_i = 1'b1;
+		#1
+		sw_green_i = 1'b0;
+		#400
+		sw_green_i = 1'b1;
+		#1
+		sw_green_i = 1'b0;
+	end
+
+	initial #2400 $finish;
+endmodule 
