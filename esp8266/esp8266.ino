8dc4c2f0763e0c8ca275a902f14e06fe7b047f19
Emmanuel Valentin <ema_110@outlook.es>
2023-11-24 01:54:43 -0600

    Practica esp8266

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -1,70 +1,70 @@
+#include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
-const char* ssid = "";
-const char* password = "";
+#include <FS.h>
+#include <LittleFS.h>  
+#include "secrets.h"
 
-WiFiServer server(80);
+ESP8266WebServer server(80);
 
-void setup() {
-  Serial.begin(115200);
-  delay(10);
-
-  // Conecta a la red wifi.
-  Serial.println();
-  Serial.println();
-  Serial.println("Conectando con");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("Conectado con WiFi");
-
-  // Inicio del Servidor web.
-  server.begin();
-  Serial.println("Servidor web iniciado");
-
-  // Esta es la IP a la cual deberas conectarte una vez se haya establecido comunicacion con el ESP. 
-  // Es decir, cuando se vea en consola la IP, deberas, conectarte desde tu navegador a esa IP.
-  Serial.print("Esta es la IP para conectar: ");
-  Serial.print("http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("/");
-}
-
-void loop() {
-  // Consulta si se ha conectado algun cliente
-  WiFiClient client = server.available();
-  if (!client) {
+void handleIndex() {
+  if (!SPIFFS.begin()) {
+    Serial.println("Error al montar el sistema de archivos");
     return;
   }
 
-  // Espera hasta que el cliente envue datos
-  Serial.println("Nuevo cliente");
-  while(!client.available()) {
-    delay(1);
+  File file = SPIFFS.open("/index.html", "r");
+  if (!file) {
+    Serial.println("Error al abrir el archivo");
+    SPIFFS.end();
+    return;
   }
 
-  // Pagina WEB 
-  client.println("HTTP/1.1 200 OK");
-  client.println("Content-Type: text/html");
-  client.println(""); // Linea importante
-  client.println("<!DOCTYPE HTML>");
-  client.println("<html>");
-  client.println("<head><meta charset=utf-8></head>");
-  client.println("<body><font face='Arial'><center><h1>Practica de Servidor web con NodeMCU.</h1>");
-  client.println("<h2><font color='#009900'>Grupo 6CV2</font></h2>");
-  client.println("<h3>Página web.</h3>");
-  client.println("<br><br>");
-  client.println("<img src='https://lagarto.ipn.mx/images/2019/10/03/img_20190905_091841_124.jpg'><br>");
-  client.println("<button>Sistemas </button>");
-  client.println("<button>Embebidos </button>");
-  client.println("<button>6CV2 </button>");
-  client.println("<a href=\"/mensaje=4\"\"><button>Práctica </button></a>");
-  client.println("</font></center></body></html>");
-  Serial.println("Cliente desconectado.");
+  String fileText = "";
 
+  while (file.available()) {
+    fileText += file.readStringUntil('\n');
+  }
+
+  server.send(200, "text/html", fileText);
+}
+
+void handleToggleLed() {
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  String response = "{\"state\":";
+
+  if (digitalRead(LED_BUILTIN) == HIGH) response += "\"off\"";
+  else response += "\"on\"";
+
+  response += "}";
+  
+  server.send(200, "text/plain", response);
+}
+
+void setup(void) {
+  Serial.begin(115200);
+
+  Serial.print("Connecting");
+  WiFi.begin(ssid, passPhrase);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(2000);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("CONNECTED");
+
+  server.on("/", HTTP_GET, handleIndex);
+  server.on("/", HTTP_POST, handleToggleLed);
+
+  server.begin();
+  Serial.print("Server IP: http://");
+  Serial.print(WiFi.localIP());
+  Serial.print("/\n");
+}
+
+
+void loop(void) {
+  server.handleClient();
+  pinMode(LED_BUILTIN, OUTPUT);
 }
---
+++ esp8266/secrets.h
@@ -0,0 +1,7 @@
+#ifndef STASSID
+#define STASSID "FAM VALENTIN 2.0"
+#define STAPSK "E2m1M8A08n"
+#endif
+
+const char *ssid = STASSID;
+const char *passPhrase = STAPSK;
